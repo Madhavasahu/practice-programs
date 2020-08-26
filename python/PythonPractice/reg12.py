7490ad0cb0410802ec25eba7c018de7288fb3113
madhavasahu <madhavasahu@gmail.com>
2020-08-26 12:00:00 +0530

    changed

--- python/PythonPractice/reg12.py
+++ python/PythonPractice/reg12.py
@@ -2,7 +2,7 @@
 
 import re
 stg = "Agent Alice gave the documents to Agent Bob"
-regex = re.compile(r'Agent (\w)(\w*)') #to get the A in Alice and B in Bob
+regex = re.compile(r'Agent (\w)\w*') #to get the A in Alice and B in Bob
 print(regex.findall(stg))
 resu = regex.sub(r'Agent \1***',stg) #replace the first group in the source string
 print(resu) #here, the \1 represent first group which is A,B and \2 represent second group
